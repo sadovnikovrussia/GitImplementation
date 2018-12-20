81850317ca348e5cc85573a5c625cf3fa33d0744
Alexey <sadovnikov@st-hld.ru>
2018-12-20 15:24:33 +0300

    Какие-то фишки от Я.

--- app/src/main/java/dev/sadovnikov/gitimplementation/MainActivity.java
+++ app/src/main/java/dev/sadovnikov/gitimplementation/MainActivity.java
@@ -1,14 +1,24 @@
 package dev.sadovnikov.gitimplementation;
 
+import android.support.v4.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyApplication myApplication = new MyApplication();
+        HelloWrold helloWrold = MyApplication.from(this).getHelloWrold();
+        Log.d(TAG, "onCreate: " + helloWrold.secret);
     }
 
     @Override
@@ -24,5 +34,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    public void getSecret() {
+        HelloWrold helloWrold = new HelloWrold();
+        Field field = null;
+        try {
+            field = helloWrold.getClass().getDeclaredField("secret");
+        } catch (NoSuchFieldException e) {
+            Log.e(TAG, "getSecret: ", e);
+        }
+        Object o = null;
+        try {
+            o = field.get(helloWrold);
+        } catch (IllegalAccessException e) {
+            Log.e(TAG, "getSecret: ", e);
+        }
+        Log.d(TAG, "getSecret: " + o);
     }
 }
